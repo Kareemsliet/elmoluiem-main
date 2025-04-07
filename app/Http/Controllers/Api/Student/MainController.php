805faee888c5e7b1169ee1bc32ef5a309a5922f2
Your Name <emmh16991@gmail.com>
2025-04-07 20:26:42 +0200

    mintstudent

--- app/Http/Controllers/Api/Student/MainController.php
+++ app/Http/Controllers/Api/Student/MainController.php
@@ -4,7 +4,8 @@ namespace App\Http\Controllers\Api\Student;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Api\Main\RatingRequest;
-
+use App\Models\Teacher;
+use App\Models\Rating;
 class MainController extends Controller
 {
     public $student;
@@ -16,17 +17,48 @@ class MainController extends Controller
 
     public function rateTeacher(RatingRequest $rquest,$teacher_id){
         $rquest->validated();
-        
+    
+
         //post
+
+
+
+$teacher = Teacher::find($teacher_id);
+if (!$teacher) {
+    
+    return failResponse("Teacher not found");
+}
+
+
+$rating = new Rating();
+$rating->student_id = $this->student->id;
+$rating->teacher_id = $teacher_id;
+$rating->rating = $request->rate; 
+$rating->description = $request->description; 
+$rating->save();
+
+
+return successResponse("Rating submitted successfully");
+
+
     }
 
     public function allGivenRatings(RatingRequest $request){
         $request->validated();
         
-        //post
+        //get
+        $ratings = Rating::where('student_id', $this->student->id)->get();
+
+        
+        return successResponse("All given ratings", $ratings);
     }
 
     public function allReceivedRatings(){
         // get
+        $ratings = Rating::where('student_id', $this->student->id)->get();
+
+        
+        return successResponse("All received ratings", $ratings);
+
     }
 }
