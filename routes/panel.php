4606bf11732a52fc0ee05cc4372c9dc0f7ff3d5c
Kareemsliet <kareemoii37@gmail.com>
2025-04-11 02:14:19 +0200

    new commit

--- routes/panel.php
+++ routes/panel.php
@@ -1,5 +1,8 @@
 <?php
 
-Route::group(["prefix"=>"admin-panel"],function(){
-    // Admin Routes
+use App\Http\Controllers\Panel\CountriesController;
+use Illuminate\Support\Facades\Route;
+
+Route::group(["prefix" => "admin-panel"], function () {
+    Route::resource("/countries", CountriesController::class);
 });
